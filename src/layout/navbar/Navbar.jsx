e8a54b79a349cabbcd5d55801b4bb08e2edeffa8
shehlaabdullayeva2004 <mi7ttwzlc@code.edu.az>
2025-11-07 16:28:22 +0400

    changes

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NavLogo from '../../assets/images/NavLogo.jpg';
 import { IoSearchOutline } from "react-icons/io5";
-import { FaBarsStaggered } from "react-icons/fa6";
+import { HiBars3 } from "react-icons/hi2";
 import { LiaTimesSolid } from "react-icons/lia";
 import { HiMiniBars3BottomRight } from "react-icons/hi2";
 
@@ -10,17 +10,140 @@ import { HiMiniBars3BottomRight } from "react-icons/hi2";
 import './Style.scss';
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [lang, setLang] = useState("az");
+  
+  const options = [
+    {
+      value: "az",
+      label: "Az",
+    },
+    {
+      value: "en",
+      label: "En",
+    },
+    {
+      value: "ru",
+      label: "Ru",
+    },
+  ];
+  const products = [
+    {
+      name: "İmplantologiya",
+      types: [
+        {
+          type: "Diş İmplantları",
+        },
+        {
+          type: "Abatmentlər və Komponentlər",
+        },
+        {
+          type: "Cərrahi Alətlər",
+        },
+        {
+          type: "Biomateriallar",
+        },
+      ],
+    },
+    {
+      name: "Diş Laboratoriyası Materialları",
+      types: [
+        {
+          type: "Protez Materialları",
+        },
+        {
+          type: "Ölçü Materialları",
+        },
+        {
+          type: "3D Çap Materialları",
+        },
+        {
+          type: "Laboratoriya Cihazları",
+        },
+      ],
+    },
+    {
+      name: "İmplantologiya",
+      types: [
+        {
+          type: "Diş İmplantları",
+        },
+        {
+          type: "Abatmentlər və Komponentlər",
+        },
+        {
+          type: "Cərrahi Alətlər",
+        },
+        {
+          type: "Biomateriallar",
+        },
+      ],
+    },
+    {
+      name: "İmplantologiya",
+      types: [
+        {
+          type: "Diş İmplantları",
+        },
+        {
+          type: "Abatmentlər və Komponentlər",
+        },
+        {
+          type: "Cərrahi Alətlər",
+        },
+        {
+          type: "Biomateriallar",
+        },
+      ],
+    },
+    {
+      name: "İmplantologiya",
+      types: [
+        {
+          type: "Diş İmplantları",
+        },
+        {
+          type: "Abatmentlər və Komponentlər",
+        },
+        {
+          type: "Cərrahi Alətlər",
+        },
+        {
+          type: "Biomateriallar",
+        },
+      ],
+    },
+    {
+      name: "İmplantologiya",
+      types: [
+        {
+          type: "Diş İmplantları",
+        },
+        {
+          type: "Abatmentlər və Komponentlər",
+        },
+        {
+          type: "Cərrahi Alətlər",
+        },
+        {
+          type: "Biomateriallar",
+        },
+      ],
+    },
+  ];
+  
   const toggleSidebar = () => setIsOpen(!isOpen);
   
   return (
     <>
-      <nav className="custom-navbar navbar-expand-lg px-2 px-md-3 px-lg-5  d-flex align-items-center justify-content-between rounded-4 overflow-hidden ">
-        <Link to="/" className="navbar-brand d-flex align-items-center">
+      <nav className="custom-navbar navbar-expand-lg px-2  px-md-3 px-lg-1  d-flex align-items-center justify-content-between">
+        <Link
+          to="/"
+          className="navbar-brand d-flex align-items-center overflow-hidden"
+        >
           <img src={NavLogo} alt="Logo" className="nav-logo" />
         </Link>
-        <div className="mx-auto d-none d-lg-flex gap-2">
+        <div className="mx-auto d-none d-lg-flex gap-2 align-items-center">
           <Link to="/" className="nav-link">
             Ana Səhifə
           </Link>
@@ -31,9 +154,31 @@ export default function Navbar() {
           <Link to="/services" className="nav-link">
             Xidmətlər
           </Link>
-          <Link to="/products" className="nav-link">
-            Məhsullar
-          </Link>
+          {/* <div className="border position-relative  g-0 m-0 p-0 gap-0"> */}
+          <div className='productDiv'>
+            <Link to="/products" className="nav-link">
+              Məhsullar
+            </Link>
+          </div>
+          <div className="products-types  mt-3 position-absolute p-0 m-0 ">
+            <div className="products-types-container  mt-3 px-5">
+              <ul className=" p-0 d-flex gap-4 ">
+                {products.map(({ name, types }, index) => (
+                  <li key={index} className=" pe-1 d-flex flex-column">
+                    <h5>{name}</h5>
+                    <div className="d-flex flex-column gap-2">
+                      {types.map(({ type }, i) => (
+                        <p key={i} className=" p-0 m-0">
+                          {type}
+                        </p>
+                      ))}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* </div> */}
           <Link to="/news" className="nav-link">
             Xəbərlər
           </Link>
@@ -44,12 +189,38 @@ export default function Navbar() {
             Əlaqə
           </Link>
         </div>
-        <div className="d-flex   justify-content-end align-items-center gap-2">
-          <div className="">
+        <div className="d-flex justify-content-end align-items-center gap-0">
+          <div className="d-none d-lg-block">
             <IoSearchOutline size={20} className="text-dark" />
           </div>
+          <div className="language-dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {options.find((o) => o.value === lang).label}
+            </button>
+            <ul
+              className="dropdown-menu dropdown-menu"
+              aria-labelledby="dropdownMenuButton"
+            >
+              {options.map((opt) => (
+                <li key={opt.value}>
+                  <button
+                    className="dropdown-item"
+                    onClick={() => setLang(opt.value)}
+                  >
+                    {opt.label}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="d-block d-lg-none  " onClick={toggleSidebar}>
-            <HiMiniBars3BottomRight size={20} className="text-dark" />
+            <HiBars3 size={20} className="text-dark" />
           </div>
         </div>
       </nav>
@@ -68,7 +239,7 @@ export default function Navbar() {
             <input type="text" name="" id="" />
           </div>
 
-          <div className='w-100 align-items-start  '>
+          <div className="w-100 align-items-start  ">
             <Link to="/" className="nav-link">
               Ana Səhifə
             </Link>
